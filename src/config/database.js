84c465661f02461c94a8e6f8192c8a6798c1a0b1
Wisnukan <28.ngakanmadewisnumahesaadnyana.com>
2024-06-01 16:30:07 +0800

    haha

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2')
 const dbPool = mysql.createPool({
     host: 'sql12.freemysqlhosting.net',
     user: 'sql12711009',
-    password: 'bdBCA6Uxua,Ow6',
+    password: 'bdBCA6Uxua',
     database: 'sql12711009',
 })
 
