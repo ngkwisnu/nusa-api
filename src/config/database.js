45117fa46e093c163702434f7a5d389ed2cf8266
Wisnukan <28.ngakanmadewisnumahesaadnyana.com>
2024-06-01 16:28:15 +0800

    haha

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2')
 
 const dbPool = mysql.createPool({
-    host: 'SG-tour-guide-apps-8584-mysql-master.servers.mongodirector.com',
-    user: 'sgroot',
-    password: 'TssrFMTJs1Bf,Ow6',
-    database: 'nusaguide',
+    host: 'sql12.freemysqlhosting.net',
+    user: 'sql12711009',
+    password: 'bdBCA6Uxua,Ow6',
+    database: 'sql12711009',
 })
 
 module.exports = dbPool.promise()
