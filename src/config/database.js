fe68ece1436825ae59bbbc9d7baa4cfe95a82523
Wisnukan <28.ngakanmadewisnumahesaadnyana.com>
2024-06-10 15:56:09 +0800

    Fixed Database Host

--- src/config/database.js
+++ src/config/database.js
@@ -2,9 +2,9 @@ const mysql = require('mysql2')
 
 const dbPool = mysql.createPool({
     host: 'sql12.freemysqlhosting.net',
-    user: 'sql12711009',
-    password: 'bdBCA6Uxua',
-    database: 'sql12711009',
+    user: 'sql12712970',
+    password: 'U5cb1ETEjs',
+    database: 'sql12712970',
 })
 
 module.exports = dbPool.promise()
